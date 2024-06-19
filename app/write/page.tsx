92ef04ebe9cc02c2a5baacedcea6645d63129e0b
Edwin Shi <edwinshi1021@hotmail.com>
2024-06-18 20:20:41 -0700

    update

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -1,13 +1,16 @@
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 
 export default function page() {
   return (
     <div className="flex">
-      <div className="flex flex-col basis-1/2 border-r min-h-[calc(100vh-65px)] p-10 gap-6">
+      <div className="flex flex-col basis-1/2 border-r min-h-[calc(100vh-65px)] p-7 gap-6">
         <h2 className="font-semibold text-lg">Markdown</h2>
         <Input className="h-14 text-lg" placeholder="Title" />
         <Textarea className="flex-1" placeholder="Typing..." />
+
+        <Button>Submit</Button>
       </div>
 
       <div className="basis-1/2 border-r min-h-[calc(100vh-65px)] p-10">
